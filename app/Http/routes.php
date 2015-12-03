d539f1db7e37e20a1acd97ec90903118d6fb4dfd
JasperAlgra <jasper@algra.io>
2015-12-03 21:21:57 +0100

    begin intergratie library

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,9 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/cgps', function () {
-    return view('cgps');
-});
+Route::get('/cgps', [
+    'uses' => 'CgpsController@receive@receive'
+]);
+
+Route::get('/graph', [
+    'middleware' => 'auth',
+    'uses' => 'Graph\GraphController@view'
+]);
+
 
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
@@ -35,3 +41,31 @@ Route::post('password/email', 'Auth\PasswordController@postEmail');
 // Password reset routes...
 Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
 Route::post('password/reset', 'Auth\PasswordController@postReset');
+
+
+Route::get('/show-autoloaders', function(){
+    foreach(spl_autoload_functions() as $callback)
+    {
+        if(is_string($callback))
+        {
+            echo '- ',$callback,"\n<br>\n";
+        }
+
+        else if(is_array($callback))
+        {
+            if(is_object($callback[0]))
+            {
+                echo '- ',get_class($callback[0]);
+            }
+            elseif(is_string($callback[0]))
+            {
+                echo '- ',$callback[0];
+            }
+            echo '::',$callback[1],"\n<br>\n";
+        }
+        else
+        {
+            var_dump($callback);
+        }
+    }
+});
