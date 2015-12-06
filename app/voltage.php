1ff14a9a3e74bdd6f403dbfd2829f24db2cdd028
Jasper <jasper@algra.io>
2015-12-06 21:17:50 +0100

    files added

--- app/voltage.php
+++ app/voltage.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Eloquent;
 
-class voltage extends Model
+class Voltage extends Eloquent
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['report_id', 'input', 'value'];
+
+
+    public function report() {
+
+        return $this->hasOne('App\Report');
+    }
 }
